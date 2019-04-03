7331fb6254ae736ab7357d9ebe3f23a7a31f0aa5
Simon Marchi <simon.marchi@efficios.com>
2019-04-03 18:36:37 -0400

    Make test_stream pass

--- tests/bindings/python/bt2/test_stream.py
+++ tests/bindings/python/bt2/test_stream.py
@@ -1,29 +1,36 @@
-from collections import OrderedDict
-from bt2 import value
 import unittest
-import bt2
+from test_utils.test_utils import run_in_component_init
 
 
-@unittest.skip("this is broken")
 class StreamTestCase(unittest.TestCase):
-    def setUp(self):
-        trace = bt2.Trace()
-        self._sc = trace.create_stream_class(automatic_stream_id=False)
-        self._stream = self._sc(id=1608)
+    @staticmethod
+    def _get_test_stream_class_and_trace():
+        def f(comp_self):
+            return comp_self._create_trace_class()
 
-    def tearDown(self):
-        del self._sc
-        del self._stream
+        tc = run_in_component_init(f)
+        sc = tc.create_stream_class(assigns_automatic_stream_id=False)
+        tr = tc()
+
+        return sc, tr
 
     def test_attr_stream_class(self):
-        self.assertEqual(self._stream.stream_class.addr, self._sc.addr)
+        sc, tr = self._get_test_stream_class_and_trace()
+        stream = tr.create_stream(sc, id=1608)
+        self.assertEqual(stream.stream_class, sc)
 
     def test_attr_name(self):
-        self._stream.name = 'my_stream'
-        self.assertEqual(self._stream.name, 'my_stream')
+        sc, tr = self._get_test_stream_class_and_trace()
+        a_stream_with_no_name = tr.create_stream(sc, id=1608)
+        self.assertEqual(a_stream_with_no_name.name, None)
+
+        a_stream_with_a_name = tr.create_stream(sc, id=1609, name='best stream')
+        self.assertEqual(a_stream_with_a_name.name, 'best stream')
 
     def test_attr_id(self):
-        self.assertEqual(self._stream.id, 1608)
+        sc, tr = self._get_test_stream_class_and_trace()
+        stream = tr.create_stream(sc, id=1608)
+        self.assertEqual(stream.id, 1608)
 
     def test_default_clock_always_known(self):
         pass
