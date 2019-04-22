0b3ff166b39e40851884ce6e6ee3c1720476cad9
Francis Deslauriers <francis.deslauriers@efficios.com>
2019-04-22 19:17:46 -0400

    bt2: tests: cleanup: create trace class in setup phase
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>

--- tests/bindings/python/bt2/test_trace.py
+++ tests/bindings/python/bt2/test_trace.py
@@ -3,35 +3,32 @@ from test_utils.test_utils import get_dummy_trace_class
 
 
 class TraceTestCase(unittest.TestCase):
+    def setUp(self):
+        self._tc = get_dummy_trace_class()
 
     def test_create_default(self):
-        tc = get_dummy_trace_class()
-        trace = tc()
+        trace = self._tc()
         self.assertEqual(trace.name, None)
 
     def test_create_full(self):
-        tc = get_dummy_trace_class()
-        trace = tc(name='my name')
+        trace = self._tc(name='my name')
         self.assertEqual(trace.name, 'my name')
 
     def test_create_invalid_name(self):
-        tc = get_dummy_trace_class()
         with self.assertRaises(TypeError):
-            trace = tc(name=17)
+            trace = self._tc(name=17)
 
     def test_len(self):
-        tc = get_dummy_trace_class()
-        trace = tc()
-        sc = tc.create_stream_class()
+        trace = self._tc()
+        sc = self._tc.create_stream_class()
         self.assertEqual(len(trace), 0)
 
         trace.create_stream(sc)
         self.assertEqual(len(trace), 1)
 
     def test_iter(self):
-        tc = get_dummy_trace_class()
-        sc = tc.create_stream_class(assigns_automatic_stream_id=False)
-        trace = tc()
+        sc = self._tc.create_stream_class(assigns_automatic_stream_id=False)
+        trace = self._tc()
         trace.create_stream(sc, id=12)
         trace.create_stream(sc, id=15)
         trace.create_stream(sc, id=17)
