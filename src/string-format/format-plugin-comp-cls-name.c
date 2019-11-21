3e981180bba867e78ae207cca8ccaaf34c020c19
Simon Marchi <simon.marchi@efficios.com>
2019-11-21 16:58:12 -0500

    Introduce shared function to format component class name
    The CLI has a function to print a component class name nicely,
    print_plugin_comp_cls_opt.  The same functionality will be needed by
    the Python bindings, move the function to a new convenience library.
    Modify the function to return a string instead of printing directly to a
    stream.  Update users accordingly.
    
    Change-Id: I83fb61107ce93497ed34ebc383cbd3185c19752c
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>

---
+++ src/string-format/format-plugin-comp-cls-name.c
@@ -0,0 +1,106 @@
+/*
+ * Copyright EfficiOS, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#define BT_LOG_OUTPUT_LEVEL log_level
+#define BT_LOG_TAG "COMMON/FORMAT-PLUGIN-COMP-CLS-NAME"
+#include <logging/log.h>
+
+#include "format-plugin-comp-cls-name.h"
+
+#include <common/common.h>
+
+
+static
+const char *component_type_str(bt_component_class_type type)
+{
+	switch (type) {
+	case BT_COMPONENT_CLASS_TYPE_SOURCE:
+		return "source";
+	case BT_COMPONENT_CLASS_TYPE_SINK:
+		return "sink";
+	case BT_COMPONENT_CLASS_TYPE_FILTER:
+		return "filter";
+	default:
+		return "(unknown)";
+	}
+}
+
+gchar *format_plugin_comp_cls_opt(const char *plugin_name,
+		const char *comp_cls_name, bt_component_class_type type)
+{
+	GString *str;
+	GString *shell_plugin_name = NULL;
+	GString *shell_comp_cls_name = NULL;
+	gchar *ret;
+
+	str = g_string_new(NULL);
+	if (!str) {
+		goto end;
+	}
+
+	if (plugin_name) {
+		shell_plugin_name = bt_common_shell_quote(plugin_name, false);
+		if (!shell_plugin_name) {
+			goto end;
+		}
+	}
+
+	shell_comp_cls_name = bt_common_shell_quote(comp_cls_name, false);
+	if (!shell_comp_cls_name) {
+		goto end;
+	}
+
+	g_string_append_printf(str, "'%s%s%s%s",
+		bt_common_color_bold(),
+		bt_common_color_fg_bright_cyan(),
+		component_type_str(type),
+		bt_common_color_fg_default());
+
+	if (shell_plugin_name) {
+		g_string_append_printf(str, ".%s%s%s",
+			bt_common_color_fg_blue(),
+			shell_plugin_name->str,
+			bt_common_color_fg_default());
+	}
+
+	g_string_append_printf(str, ".%s%s%s'",
+		bt_common_color_fg_yellow(),
+		shell_comp_cls_name->str,
+		bt_common_color_reset());
+
+end:
+	if (shell_plugin_name) {
+		g_string_free(shell_plugin_name, TRUE);
+	}
+
+	if (shell_comp_cls_name) {
+		g_string_free(shell_comp_cls_name, TRUE);
+	}
+
+	if (str) {
+		ret = g_string_free(str, FALSE);
+	} else {
+		ret = NULL;
+	}
+
+	return ret;
+}
