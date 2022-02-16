efecbad0329f97f6ed4442de61dafce8187341d3
Simon Marchi <simon.marchi@efficios.com>
2022-02-16 12:23:23 -0500

    lib: rename bt_resolve_field_path_context to bt_resolve_field_xref_context
    In preparation to sharing some code with the field location resolving,
    rename bt_resolve_field_path_context to bt_resolve_field_xref_context,
    which will be used by both kinds of field cross-reference resolving.
    Move it to its own file, which will be included by both.
    
    Change-Id: Ia6b7e9fa9de76d16fa0244a66597c3a1bfb30271
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7303
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ src/lib/trace-ir/resolve-field-xref.h
@@ -0,0 +1,19 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2016-2018 Philippe Proulx <pproulx@efficios.com>
+ */
+
+#ifndef BABELTRACE_TRACE_IR_RESOLVE_FIELD_XREF
+#define BABELTRACE_TRACE_IR_RESOLVE_FIELD_XREF
+
+#include "lib/func-status.h"
+
+struct bt_resolve_field_xref_context {
+	struct bt_field_class *packet_context;
+	struct bt_field_class *event_common_context;
+	struct bt_field_class *event_specific_context;
+	struct bt_field_class *event_payload;
+};
+
+#endif /* BABELTRACE_TRACE_IR_RESOLVE_FIELD_XREF */
