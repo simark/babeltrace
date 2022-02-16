281017273a28ecd9cfdbb26e762f81ae011c994e
Simon Marchi <simon.marchi@efficios.com>
2022-02-16 12:28:53 -0500

    lib: add enum bt_resolve_field_xref_status
    Add the bt_resolve_field_xref_status enumeration, and use it throughout
    the resolve-field-path.c file to make error handling more explicit, and
    clearer, in my opinion.
    
    Change-Id: I51ddb22ac41dde0f58ca2e5d7fb90267984412e9
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7304
    Tested-by: jenkins <jenkins@lttng.org>
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/lib/trace-ir/event-class.c
+++ src/lib/trace-ir/event-class.c
@@ -289,7 +289,8 @@ bt_event_class_set_specific_context_field_class(
 		struct bt_event_class *event_class,
 		struct bt_field_class *field_class)
 {
-	int ret;
+	enum bt_event_class_set_field_class_status status;
+	enum bt_resolve_field_xref_status resolve_status;
 	struct bt_stream_class *stream_class;
 	struct bt_resolve_field_xref_context resolve_ctx = {
 		.packet_context = NULL,
@@ -310,14 +311,9 @@ bt_event_class_set_specific_context_field_class(
 	resolve_ctx.event_common_context =
 		stream_class->event_common_context_fc;
 
-	ret = bt_resolve_field_paths(field_class, &resolve_ctx, __func__);
-	if (ret) {
-		/*
-		 * This is the only reason for which
-		 * bt_resolve_field_paths() can fail: anything else
-		 * would be because a precondition is not satisfied.
-		 */
-		ret = BT_FUNC_STATUS_MEMORY_ERROR;
+	resolve_status = bt_resolve_field_paths(field_class, &resolve_ctx, __func__);
+	if (resolve_status != BT_RESOLVE_FIELD_XREF_STATUS_OK) {
+		status = (int) resolve_status;
 		goto end;
 	}
 
@@ -329,8 +325,10 @@ bt_event_class_set_specific_context_field_class(
 	BT_LIB_LOGD("Set event class's specific context field class: %!+E",
 		event_class);
 
+	status = BT_EVENT_CLASS_SET_FIELD_CLASS_STATUS_OK;
+
 end:
-	return ret;
+	return status;
 }
 
 const struct bt_field_class *bt_event_class_borrow_payload_field_class_const(
@@ -352,7 +350,8 @@ bt_event_class_set_payload_field_class(
 		struct bt_event_class *event_class,
 		struct bt_field_class *field_class)
 {
-	int ret;
+	enum bt_event_class_set_field_class_status status;
+	enum bt_resolve_field_xref_status resolve_status;
 	struct bt_stream_class *stream_class;
 	struct bt_resolve_field_xref_context resolve_ctx = {
 		.packet_context = NULL,
@@ -373,14 +372,9 @@ bt_event_class_set_payload_field_class(
 		stream_class->event_common_context_fc;
 	resolve_ctx.event_specific_context = event_class->specific_context_fc;
 
-	ret = bt_resolve_field_paths(field_class, &resolve_ctx, __func__);
-	if (ret) {
-		/*
-		 * This is the only reason for which
-		 * bt_resolve_field_paths() can fail: anything else
-		 * would be because a precondition is not satisfied.
-		 */
-		ret = BT_FUNC_STATUS_MEMORY_ERROR;
+	resolve_status = bt_resolve_field_paths(field_class, &resolve_ctx, __func__);
+	if (resolve_status != BT_RESOLVE_FIELD_XREF_STATUS_OK) {
+		status = (int) resolve_status;
 		goto end;
 	}
 
@@ -391,8 +385,10 @@ bt_event_class_set_payload_field_class(
 	bt_field_class_freeze(field_class);
 	BT_LIB_LOGD("Set event class's payload field class: %!+E", event_class);
 
+	status = BT_EVENT_CLASS_SET_FIELD_CLASS_STATUS_OK;
+
 end:
-	return ret;
+	return status;
 }
 
 BT_HIDDEN
--- src/lib/trace-ir/resolve-field-path.h
+++ src/lib/trace-ir/resolve-field-path.h
@@ -14,7 +14,7 @@
 #include "resolve-field-xref.h"
 
 BT_HIDDEN
-int bt_resolve_field_paths(
+enum bt_resolve_field_xref_status bt_resolve_field_paths(
 		struct bt_field_class *field_class,
 		struct bt_resolve_field_xref_context *ctx,
 		const char *api_func);
