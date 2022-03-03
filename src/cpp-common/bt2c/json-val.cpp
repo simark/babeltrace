9082fc3ae11e089c2583a1f2ec0b3955da9e34a6
Philippe Proulx <eeppeliteloop@gmail.com>
2022-03-03 13:34:44 -0500

    src/cpp-common: make `bt2c::JsonVal` visitable
    This patch adds the bt2c::JsonVal::accept() method which accepts a JSON
    value visitor (`bt2c::JsonValVisitor`), dispatching to the virtual
    bt2c::JsonVal::_accept() method.
    
    This is part of an effort to support CTF2‑SPECRC‑4.0 [1].
    
    [1]: https://diamon.org/ctf/files/CTF2-SPECRC-4.0.html
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I75462799f9433ee868b618ecba4ba63b5822b96c
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7485
    Reviewed-by: Simon Marchi <simon.marchi@efficios.com>

--- src/cpp-common/bt2c/json-val.cpp
+++ src/cpp-common/bt2c/json-val.cpp
@@ -65,8 +65,18 @@ const JsonObjVal& JsonVal::asObj() const noexcept
     return static_cast<const JsonObjVal&>(*this);
 }
 
+void JsonVal::accept(JsonValVisitor& visitor) const
+{
+    this->_accept(visitor);
+}
+
 JsonNullVal::JsonNullVal(TextLoc loc) noexcept : JsonVal {Type::NUL, std::move(loc)}
 {
+}
+
+void JsonNullVal::_accept(JsonValVisitor& visitor) const
+{
+    visitor.visit(*this);
 }
 
 #ifdef BT_DEBUG_MODE
@@ -100,6 +110,11 @@ JsonArrayVal::JsonArrayVal(Container&& vals, TextLoc loc) :
 #endif
 }
 
+void JsonArrayVal::_accept(JsonValVisitor& visitor) const
+{
+    visitor.visit(*this);
+}
+
 JsonObjVal::JsonObjVal(Container&& vals, TextLoc loc) :
     JsonCompoundVal {std::move(vals), std::move(loc)}
 {
@@ -108,6 +123,11 @@ JsonObjVal::JsonObjVal(Container&& vals, TextLoc loc) :
         return elem.second;
     }));
 #endif
+}
+
+void JsonObjVal::_accept(JsonValVisitor& visitor) const
+{
+    visitor.visit(*this);
 }
 
 JsonNullVal::UP createJsonVal(TextLoc loc)
