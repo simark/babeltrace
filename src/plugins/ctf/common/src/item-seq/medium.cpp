10dd1e69f2ff8403034c033eaf0d512e0c8a1222
Philippe Proulx <eeppeliteloop@gmail.com>
2022-04-25 11:16:46 -0400

    src/plugins/ctf/common/src: add item sequence iterator API
    This patch adds the item sequence iterator API, internal to the `ctf`
    plugin, to be used by the source components (`src.ctf.fs` and
    `sink.ctf.fs`).
    
    An item sequence iterator can decode a sequence of packets using:
    
    * A medium, which provides binary stream data to the iterator.
    * A CTF IR trace class, which describes how to decode said data.
    
    This makes it possible to completely decode CTF data streams without
    ever needing to create trace IR objects. In other words, it's a
    standalone CTF decoding library which, minus a few utilities borrowed
    from the Babeltrace project, could even be reused in other projects.
    
    The value of an item sequence iterator is an item. The item sequence
    iterator doesn't actually create items as it advances: it has one
    instance of each concrete item class and sets a pointer to one of
    those as it advances (it's a single-pass input iterator).
    
    Seek a specific packet beginning with the seekPkt() method.
    
    Methods which make the iterator decode may throw `DecodingError`.
    
    NOTE: Currently, this isn't an STL-compliant iterator class: you
    can't copy an instance and you can't get an end iterator, because
    there's no item sequence (container) class (you need to use the
    isEnded() method). Also, the typical STL type aliases such as `value`
    and `reference` aren't part of the class.
    
    This API and its implementation are inspired by the yactfr
    (<https://github.com/eepp/yactfr>) element sequence iterator API,
    conveniently written by the same author.
    
    Please read the class comments of `ctf::src::ItemSeqIter` and
    `ctf::src::Medium` to learn how to use this API and how it works
    internally.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I423cc934d2425331a09f16ba40fbeb6b5496ff3a
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7966

---
+++ src/plugins/ctf/common/src/item-seq/medium.cpp
@@ -0,0 +1,27 @@
+/*
+ * Copyright (c) 2022 Philippe Proulx <pproulx@efficios.com>
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "common/assert.h"
+#include "medium.hpp"
+
+namespace ctf {
+namespace src {
+
+Buf::Buf(const std::uint8_t * const addr, const bt2_common::DataLen size) noexcept :
+    _mAddr {addr}, _mSize {size}
+{
+    BT_ASSERT_DBG(!size.hasExtraBits());
+}
+
+Buf Buf::slice(const bt2_common::DataLen offset) const noexcept
+{
+    BT_ASSERT_DBG(offset <= _mSize);
+    BT_ASSERT_DBG(!offset.hasExtraBits());
+    return Buf {_mAddr + offset.bytes(), _mSize - offset};
+}
+
+} /* namespace src */
+} /* namespace ctf */
