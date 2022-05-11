d01f0885a80cf9e629109bc47c817a7e4f1bc5fb
Francis Deslauriers <francis.deslauriers@efficios.com>
2022-05-11 18:00:24 -0400

    Cleanup: src.ctf.fs: remove unused decoder functions
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Change-Id: I8c2610883c8730a36e3e4336c2dc48b67d3e6236
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8038
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/plugins/ctf/common/src/metadata/tsdl/decoder.hpp
+++ src/plugins/ctf/common/src/metadata/tsdl/decoder.hpp
@@ -136,22 +136,6 @@ int ctf_metadata_decoder_is_packetized(FILE *fp, bool *is_packetized, int *byte_
                                        const ctf::LogCfg& logCfg);
 
 /*
- * Returns the byte order of the decoder's metadata stream as set by the
- * last call to ctf_metadata_decoder_append_content().
- *
- * Returns -1 if unknown (plain text content).
- */
-BT_HIDDEN
-int ctf_metadata_decoder_get_byte_order(struct ctf_metadata_decoder *mdec);
-
-/*
- * Returns the UUID of the decoder's metadata stream as set by the last
- * call to ctf_metadata_decoder_append_content().
- */
-BT_HIDDEN
-int ctf_metadata_decoder_get_uuid(struct ctf_metadata_decoder *mdec, bt_uuid_t uuid);
-
-/*
  * Returns the UUID of the decoder's trace class, if available.
  *
  * Returns:
