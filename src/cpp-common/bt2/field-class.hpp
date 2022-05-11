aa924a0d8459e0cc415e549f66d27db1ee654a00
Philippe Proulx <eeppeliteloop@gmail.com>
2022-05-11 12:26:49 -0400

    src/cpp-common/bt2: wrap variant with selector field loc. FC API
    This patch adds what's needed to the C++ libbabeltrace2 wrappers to
    wrap:
    
    * bt_field_class_variant_with_selector_field_borrow_selector_field_location_const()
    * bt_field_class_variant_with_selector_field_location_integer_unsigned_create()
    * bt_field_class_variant_with_selector_field_location_integer_signed_create()
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: Id644771aa9537be21d73ba6cfe97a287c19f7240
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8019

--- src/cpp-common/bt2/field-class.hpp
+++ src/cpp-common/bt2/field-class.hpp
@@ -2429,6 +2429,13 @@ public:
                 this->libObjPtr())};
     }
 
+    ConstFieldLocation selectorFieldLocation() const noexcept
+    {
+        return ConstFieldLocation {
+            bt_field_class_variant_with_selector_field_borrow_selector_field_location_const(
+                this->libObjPtr())};
+    }
+
     Shared shared() const noexcept
     {
         return Shared {*this};
