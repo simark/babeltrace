2a6fd8e658f2a11c717a268c286ad1d6621ab662
Philippe Proulx <eeppeliteloop@gmail.com>
2022-05-17 14:09:05 -0400

    field-class.hpp: support optional variant FC option name
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I4b2df4343a0f773b82e472b1138536440279e940
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8056

--- src/cpp-common/bt2/field-class.hpp
+++ src/cpp-common/bt2/field-class.hpp
@@ -2172,9 +2172,15 @@ public:
         return *this;
     }
 
-    bpstd::string_view name() const noexcept
+    nonstd::optional<bpstd::string_view> name() const noexcept
     {
-        return bt_field_class_variant_option_get_name(this->libObjPtr());
+        const auto name = bt_field_class_variant_option_get_name(this->libObjPtr());
+
+        if (name) {
+            return name;
+        }
+
+        return nonstd::nullopt;
     }
 
     ConstFieldClass fieldClass() const noexcept
@@ -2304,7 +2310,7 @@ public:
         return ConstVariantFieldClassOption {_Spec::asBaseOption(this->libObjPtr())};
     }
 
-    bpstd::string_view name() const noexcept
+    nonstd::optional<bpstd::string_view> name() const noexcept
     {
         return this->asBaseOption().name();
     }
@@ -2546,9 +2552,9 @@ public:
         }
     }
 
-    void appendOption(const std::string& name, const FieldClass& fc)
+    void appendOption(const nonstd::optional<std::string>& name, const FieldClass& fc)
     {
-        this->appendOption(name.data(), fc);
+        this->appendOption(name ? name->data() : nullptr, fc);
     }
 
     Shared shared() const noexcept
@@ -2781,9 +2787,10 @@ public:
         }
     }
 
-    void appendOption(const std::string& name, const FieldClass& fc)
+    void appendOption(const nonstd::optional<std::string>& name, const FieldClass& fc,
+                      const typename Option::RangeSet& ranges)
     {
-        this->appendOption(name.data(), fc);
+        this->appendOption(name ? name->data() : nullptr, fc, ranges);
     }
 
     Iterator begin() const noexcept
