4eaf322f862b92d3d51afd088de23962c96688ad
Simon Marchi <simon.marchi@efficios.com>
2022-06-09 09:31:13 -0400

    src.ctf.fs: make ctf_fs_ds_file_group::stream a bt2::Stream::Shared
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Change-Id: I0f84b26ccf43b0469da15946fa825a1255e7ab3d
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8256
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/plugins/ctf/fs-src/fs.cpp
+++ src/plugins/ctf/fs-src/fs.cpp
@@ -1868,18 +1868,20 @@ static int create_streams_for_trace(struct ctf_fs_trace *ctf_fs_trace)
         BT_ASSERT(ds_file_group->sc->ir_sc);
         BT_ASSERT(ctf_fs_trace->trace);
 
+        bt_stream *stream;
+
         if (ds_file_group->stream_id == UINT64_C(-1)) {
             /* No stream ID: use 0 */
-            ds_file_group->stream = bt_stream_create_with_id(
-                ds_file_group->sc->ir_sc, ctf_fs_trace->trace, ctf_fs_trace->next_stream_id);
+            stream = bt_stream_create_with_id(ds_file_group->sc->ir_sc, ctf_fs_trace->trace,
+                                              ctf_fs_trace->next_stream_id);
             ctf_fs_trace->next_stream_id++;
         } else {
             /* Specific stream ID */
-            ds_file_group->stream = bt_stream_create_with_id(
-                ds_file_group->sc->ir_sc, ctf_fs_trace->trace, (uint64_t) ds_file_group->stream_id);
+            stream = bt_stream_create_with_id(ds_file_group->sc->ir_sc, ctf_fs_trace->trace,
+                                              (uint64_t) ds_file_group->stream_id);
         }
 
-        if (!ds_file_group->stream) {
+        if (!stream) {
             BT_COMP_LOGE_APPEND_CAUSE(logCfg.selfComp,
                                       "Cannot create stream for DS file group: "
                                       "addr=%p, stream-name=\"%s\"",
@@ -1887,12 +1889,14 @@ static int create_streams_for_trace(struct ctf_fs_trace *ctf_fs_trace)
             goto error;
         }
 
-        ret = bt_stream_set_name(ds_file_group->stream, name->str);
+        ds_file_group->stream = bt2::Stream::Shared::createWithoutRef(stream);
+
+        ret = bt_stream_set_name((*ds_file_group->stream)->libObjPtr(), name->str);
         if (ret) {
             BT_COMP_LOGE_APPEND_CAUSE(logCfg.selfComp,
                                       "Cannot set stream's name: "
                                       "addr=%p, stream-name=\"%s\"",
-                                      ds_file_group->stream, name->str);
+                                      (*ds_file_group->stream)->libObjPtr(), name->str);
             goto error;
         }
 
