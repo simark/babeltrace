3ce0f73b9ffc8af0bcd9d1971d3bf1f67beef43e
Simon Marchi <simon.marchi@efficios.com>
2022-06-09 11:32:46 -0400

    src.ctf.fs: use std::string in path_is_ctf_trace
    Change-Id: I796d135f7daee29a4481e61cf073e8bbd2e0f05f
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8292
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/plugins/ctf/fs-src/fs.cpp
+++ src/plugins/ctf/fs-src/fs.cpp
@@ -784,23 +784,18 @@ end:
 
 static int path_is_ctf_trace(const char *path)
 {
-    GString *metadata_path = g_string_new(NULL);
     int ret = 0;
 
-    if (!metadata_path) {
-        ret = -1;
-        goto end;
-    }
+    std::string metadata_path = path;
+    metadata_path += G_DIR_SEPARATOR;
+    metadata_path += CTF_FS_METADATA_FILENAME;
 
-    g_string_printf(metadata_path, "%s" G_DIR_SEPARATOR_S "%s", path, CTF_FS_METADATA_FILENAME);
-
-    if (g_file_test(metadata_path->str, G_FILE_TEST_IS_REGULAR)) {
+    if (g_file_test(metadata_path.c_str(), G_FILE_TEST_IS_REGULAR)) {
         ret = 1;
         goto end;
     }
 
 end:
-    g_string_free(metadata_path, TRUE);
     return ret;
 }
 
