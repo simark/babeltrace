3d44ac43e07d080eebc49a3c5d9272cabbbb4c45
Simon Marchi <simon.marchi@efficios.com>
2022-06-13 12:47:53 -0400

    src.ctf.fs: initialize ctf_fs_ds_index_entry::packet_seq_num to UINT64_MAX in class
    Initialize the field directly to the right value.  The field was
    originally initialized to 0 to mimic the g_new0 behavior.
    
    Change-Id: Ia1df1be730b77ca0d69ae05dae12bd60ef9e9ed8
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>

--- src/plugins/ctf/fs-src/data-stream-file.cpp
+++ src/plugins/ctf/fs-src/data-stream-file.cpp
@@ -400,8 +400,6 @@ static ctf_fs_ds_index_entry::UP ctf_fs_ds_index_entry_create(const bt2_common::
     ctf_fs_ds_index_entry::UP entry =
         bt2_common::makeUnique<ctf_fs_ds_index_entry>(offset, packetSize);
 
-    entry->packet_seq_num = UINT64_MAX;
-
     return entry;
 }
 
