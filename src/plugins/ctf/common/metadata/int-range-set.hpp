ee1b01687eae8ace4c402dfd92fac5c0d79b2552
Simon Marchi <simon.marchi@efficios.com>
2023-11-03 15:44:35 +0000

    `ctf` plugin: add `ctf::IntRangeSet` class template
    This new class template is conceptually the same as the libbabeltrace2
    equivalent (`bt2::CommonIntegerRangeSet`), but it's templated and avoids
    having to make libbabeltrace2 function calls to access data.
    
    That being said, the contains() and intersects() methods don't have
    their libbabeltrace2 equivalent.
    
    Signed-off-by: Philippe Proulx <eeppeliteloop@gmail.com>
    Change-Id: I494dd1b236463eaf7c24d1143ed21afdd5e498b0
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/7923
    Reviewed-by: Francis Deslauriers <francis.deslauriers@efficios.com>

---
+++ src/plugins/ctf/common/metadata/int-range-set.hpp
@@ -0,0 +1,135 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright 2022 Philippe Proulx <pproulx@efficios.com>
+ */
+
+#ifndef _CTF_INT_RANGE_SET_HPP
+#define _CTF_INT_RANGE_SET_HPP
+
+#include <set>
+#include <utility>
+
+#include "common/assert.h"
+
+#include "int-range.hpp"
+
+namespace ctf {
+
+/*
+ * An integer range set is a set of integer ranges of which the values
+ * are of type `ValT`.
+ */
+template <typename ValT>
+class IntRangeSet final
+{
+public:
+    /* Type of the lower and upper values of contained integer ranges */
+    using Val = ValT;
+
+    /* Type of contained integer ranges */
+    using Range = IntRange<ValT>;
+
+    /* Type of the set containing the ranges */
+    using Set = std::set<Range>;
+
+public:
+    /*
+     * Builds an empty integer range set.
+     */
+    explicit IntRangeSet()
+    {
+    }
+
+    /*
+     * Builds an integer range set containing the integer ranges
+     * `ranges`.
+     */
+    explicit IntRangeSet(Set ranges) : _mRanges {std::move(ranges)}
+    {
+    }
+
+    /* Default copy/move operations */
+    IntRangeSet(const IntRangeSet&) = default;
+    IntRangeSet(IntRangeSet&&) = default;
+    IntRangeSet& operator=(const IntRangeSet&) = default;
+    IntRangeSet& operator=(IntRangeSet&&) = default;
+
+    /*
+     * Ranges of this integer range set.
+     */
+    const Set& ranges() const noexcept
+    {
+        return _mRanges;
+    }
+
+    /*
+     * Range set iterator at the first range of this set.
+     */
+    typename Set::const_iterator begin() const noexcept
+    {
+        return _mRanges.begin();
+    }
+
+    /*
+     * Range set iterator \em after the last range of this set.
+     */
+    typename Set::const_iterator end() const noexcept
+    {
+        return _mRanges.end();
+    }
+
+    /*
+     * Returns whether or not this integer range set contains the
+     * value `val` in at least one of its ranges.
+     */
+    bool contains(const Val val) const noexcept
+    {
+        for (auto& range : _mRanges) {
+            if (range.contains(val)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /*
+     * Returns whether or not at least one range contained in `other`
+     * intersects with at least one range contained in this integer
+     * range set.
+     */
+    bool intersects(const IntRangeSet& other) const noexcept
+    {
+        for (auto& range : _mRanges) {
+            for (auto& otherRange : other.ranges()) {
+                if (range.intersects(otherRange)) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    bool operator==(const IntRangeSet& other) const
+    {
+        return other.ranges() == _mRanges;
+    }
+
+    bool operator!=(const IntRangeSet& other) const
+    {
+        return !(*this == other);
+    }
+
+private:
+    Set _mRanges;
+};
+
+/* Convenient aliases */
+using UIntRangeSet = IntRangeSet<unsigned long long>;
+using SIntRangeSet = IntRangeSet<long long>;
+
+} /* namespace ctf */
+
+#endif /* _CTF_IR_HPP */
