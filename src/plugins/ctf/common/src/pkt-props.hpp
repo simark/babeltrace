399bc98ffdcc9cbc4ce57af28cd2a988241d27dd
Simon Marchi <simon.marchi@efficios.com>
2023-11-03 19:28:44 +0000

    src.ctf.fs: Add `readPktProps()`
    Signed-off-by: Francis Deslauriers <francis.deslauriers@efficios.com>
    Change-Id: I0e98db658cc50e44bc8d77c8dea53c1984af28aa
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8190
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

---
+++ src/plugins/ctf/common/src/pkt-props.hpp
@@ -0,0 +1,44 @@
+/*
+ * SPDX-License-Identifier: MIT
+ *
+ * Copyright (c) 2022 EfficiOS Inc. and Linux Foundation
+ */
+
+#ifndef _CTF_SRC_PKT_PROPS_HPP
+#define _CTF_SRC_PKT_PROPS_HPP
+
+#include "cpp-common/bt2c/data-len.hpp"
+#include "cpp-common/bt2c/logging.hpp"
+#include "cpp-common/bt2s/optional.hpp"
+
+#include "item-seq/medium.hpp"
+#include "metadata/ctf-ir.hpp"
+
+namespace ctf {
+namespace src {
+
+struct PktProps final
+{
+    bt2s::optional<bt2c::DataLen> expectedTotalLen;
+    bt2s::optional<bt2c::DataLen> expectedContentLen;
+    const DataStreamCls *dataStreamCls;
+    bt2s::optional<unsigned long long> dataStreamId;
+
+    struct
+    {
+        bt2s::optional<unsigned long long> discEventRecordCounter;
+        bt2s::optional<unsigned long long> beginDefClk;
+        bt2s::optional<unsigned long long> endDefClk;
+    } snapshots;
+};
+
+/*
+ * Extract packet properties at offset.
+ */
+PktProps readPktProps(const TraceCls& traceCls, Medium::UP medium, bt2c::DataLen pktOffset,
+                      const bt2c::Logger& parentLogger);
+
+} /* namespace src */
+} /* namespace ctf */
+
+#endif /* _CTF_SRC_PKT_PROPS_HPP */
