9dd3996ecfd1a64d4731ebd256935b60bad9866f
Simon Marchi <simon.marchi@efficios.com>
2023-11-10 16:32:38 +0000

    tests: adjust src.ctf.fs succeed tests to read CTF 2 traces
    Change-Id: I3a78fb1641c3c1bae98be74b741ce2f4b8089ec0
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8509
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- tests/plugins/src.ctf.fs/succeed/test-succeed.sh
+++ tests/plugins/src.ctf.fs/succeed/test-succeed.sh
@@ -24,10 +24,16 @@ source "$UTILSSH"
 
 this_dir_relative="plugins/src.ctf.fs/succeed"
 this_dir_build="$BT_TESTS_BUILDDIR/$this_dir_relative"
-succeed_trace_dir="$BT_CTF_TRACES_PATH/1/succeed"
 expect_dir="$BT_TESTS_DATADIR/$this_dir_relative"
 
 test_ctf_common_details_args=("-p" "with-trace-name=no,with-stream-name=no")
+
+succeed_trace_path() {
+	name="$1"
+	ctf_version="$2"
+
+	echo "$BT_CTF_TRACES_PATH/$ctf_version/succeed/$name"
+}
 
 test_ctf_gen_single() {
 	name="$1"
@@ -40,47 +46,57 @@ test_ctf_gen_single() {
 }
 
 test_ctf_single() {
-	name="$1"
+	local name="$1"
 
-	bt_diff_details_ctf_single "$expect_dir/trace-$name.expect" \
-		"$succeed_trace_dir/$name" "${test_ctf_common_details_args[@]}"
-	ok $? "Trace '$name' gives the expected output"
+	for ctf_version in 1 2; do
+		local trace_path
+
+		trace_path=$(succeed_trace_path "$name" "$ctf_version")
+
+		bt_diff_details_ctf_single "$expect_dir/trace-$name.expect" \
+			"$trace_path" "${test_ctf_common_details_args[@]}"
+		ok $? "Trace '$name' gives the expected output - CTF $ctf_version"
+	done
 }
 
 test_packet_end() {
 	local name="$1"
-	local expected_stdout="$expect_dir/trace-$name.expect"
-	local ret=0
-	local ret_stdout
-	local ret_stderr
-	local details_comp=("-c" "sink.text.details")
-	local details_args=("-p" "with-trace-name=no,with-stream-name=no,with-metadata=no,compact=yes")
-	local temp_stdout_output_file
-	local temp_greped_stdout_output_file
-	local temp_stderr_output_file
 
-	temp_stdout_output_file="$(mktemp -t actual-stdout.XXXXXX)"
-	temp_greped_stdout_output_file="$(mktemp -t greped-stdout.XXXXXX)"
-	temp_stderr_output_file="$(mktemp -t actual-stderr.XXXXXX)"
+	for ctf_version in 1 2; do
+		local expected_stdout="$expect_dir/trace-$name.expect"
+		local trace_path
+		local ret=0
+		local ret_stdout
+		local ret_stderr
+		local details_comp=("-c" "sink.text.details")
+		local details_args=("-p" "with-trace-name=no,with-stream-name=no,with-metadata=no,compact=yes")
+		local temp_stdout_output_file
+		local temp_greped_stdout_output_file
+		local temp_stderr_output_file
 
-	bt_cli "$temp_stdout_output_file" "$temp_stderr_output_file" \
-		"$succeed_trace_dir/$name" "${details_comp[@]}" \
-		"${details_args[@]}"
+		trace_path="$(succeed_trace_path "$name" "$ctf_version")"
+		temp_stdout_output_file="$(mktemp -t actual-stdout.XXXXXX)"
+		temp_greped_stdout_output_file="$(mktemp -t greped-stdout.XXXXXX)"
+		temp_stderr_output_file="$(mktemp -t actual-stderr.XXXXXX)"
 
-	bt_grep "Packet end" "$temp_stdout_output_file" > "$temp_greped_stdout_output_file"
+		bt_cli "$temp_stdout_output_file" "$temp_stderr_output_file" \
+		  "$trace_path" "${details_comp[@]}" "${details_args[@]}"
 
-	bt_diff "$expected_stdout" "$temp_greped_stdout_output_file"
-	ret_stdout=$?
+		bt_grep "Packet end" "$temp_stdout_output_file" > "$temp_greped_stdout_output_file"
 
-	bt_diff /dev/null "$temp_stderr_output_file"
-	ret_stderr=$?
+		bt_diff "$expected_stdout" "$temp_greped_stdout_output_file"
+		ret_stdout=$?
 
-	if ((ret_stdout != 0 || ret_stderr != 0)); then
-		ret=1
-	fi
+		bt_diff /dev/null "$temp_stderr_output_file"
+		ret_stderr=$?
 
-	ok $ret "Trace '$name' gives the expected output"
-	rm -f "$temp_stdout_output_file" "$temp_stderr_output_file" "$temp_greped_stdout_output_file"
+		if ((ret_stdout != 0 || ret_stderr != 0)); then
+			ret=1
+		fi
+
+		ok $ret "Trace '$name' gives the expected output - CTF $ctf_version"
+		rm -f "$temp_stdout_output_file" "$temp_stderr_output_file" "$temp_greped_stdout_output_file"
+	done
 }
 
 test_force_origin_unix_epoch() {
@@ -100,8 +116,8 @@ test_force_origin_unix_epoch() {
 	temp_stderr_output_file="$(mktemp -t actual-stderr.XXXXXX)"
 
 	bt_cli "$temp_stdout_output_file" "$temp_stderr_output_file" \
-		"$succeed_trace_dir/$name1" "${src_ctf_fs_args[@]}" \
-		"$succeed_trace_dir/$name2" "${src_ctf_fs_args[@]}" \
+		"$(succeed_trace_path "$name1" 1)" "${src_ctf_fs_args[@]}" \
+		"$(succeed_trace_path "$name2" 1)" "${src_ctf_fs_args[@]}" \
 		"${details_comp[@]}" "${details_args[@]}"
 
 	bt_diff "$expected_stdout" "$temp_stdout_output_file"
@@ -125,7 +141,7 @@ test_force_origin_unix_epoch() {
 	rm -f "$temp_stdout_output_file" "$temp_stderr_output_file"
 }
 
-plan_tests 13
+plan_tests 23
 
 test_force_origin_unix_epoch 2packets barectf-event-before-packet
 test_ctf_gen_single simple
