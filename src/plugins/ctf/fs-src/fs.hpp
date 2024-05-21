05e8d0ccd4df3af92d9097d693ec7a34af97c70d
Simon Marchi <simon.marchi@efficios.com>
2024-05-21 13:06:21 -0400

    src.ctf.fs: use CTF2 metadata stream parser
    Change-Id: Ieae140125ac14595845739534d6bb0ee65ae114d
    Signed-off-by: Simon Marchi <simon.marchi@efficios.com>
    Reviewed-on: https://review.lttng.org/c/babeltrace/+/8372
    Reviewed-by: Philippe Proulx <eeppeliteloop@gmail.com>

--- src/plugins/ctf/fs-src/fs.hpp
+++ src/plugins/ctf/fs-src/fs.hpp
@@ -32,7 +32,7 @@ struct ctf_fs_trace
     explicit ctf_fs_trace(const ctf::src::ClkClsCfg& clkClsCfg,
                           const bt2::OptionalBorrowedObject<bt2::SelfComponent> selfComp,
                           const bt2c::Logger& parentLogger) :
-        logger {parentLogger, "PLUGIN/SRC.CTF.FS/TRACE"},
+        _mLogger {parentLogger, "PLUGIN/SRC.CTF.FS/TRACE"},
         _mClkClsCfg {clkClsCfg}, _mSelfComp {selfComp}
     {
     }
@@ -52,10 +52,8 @@ struct ctf_fs_trace
 
     void parseMetadata(const bt2c::ConstBytes buffer)
     {
-        _mParseRet = ctf::src::parseMetadataStream(_mClkClsCfg, _mSelfComp, buffer, this->logger);
+        _mParseRet = ctf::src::parseMetadataStream(_mClkClsCfg, _mSelfComp, buffer, _mLogger);
     }
-
-    bt2c::Logger logger;
 
     bt2::Trace::Shared trace;
 
@@ -67,6 +65,7 @@ struct ctf_fs_trace
     uint64_t next_stream_id = 0;
 
 private:
+    bt2c::Logger _mLogger;
     ctf::src::ClkClsCfg _mClkClsCfg;
     bt2::OptionalBorrowedObject<bt2::SelfComponent> _mSelfComp;
     bt2s::optional<ctf::src::MetadataStreamParser::ParseRet> _mParseRet;
